be69cd85c7ce9ebefbf276da27b40eaf0c4a12d5
GusFontana404 <117466366+GusFontana404@users.noreply.github.com>
2023-07-28 21:25:12 -0300

    Arreglo problemas de merge

--- descargar.sh
+++ descargar.sh
@@ -3,12 +3,7 @@
 wget -qO "/app/archivos_generados/archivo.txt" https://raw.githubusercontent.com/adalessandro/EdP-2023-TP-Final/main/dict.csv
 generar_nombres(){
         archivo=$1
-<<<<<<< HEAD
         nombre=$(shuf -n 1 "$archivo"|cut -d "," -f 1|tr -s " "tr " " "_")
-=======
-        nombre=$(shuf -n 1 "$archivo"|cut -d "," -f 1|tr -s " "|tr " " "_")
->>>>>>> rama_descargar
-        echo "$nombre"
 }
 
 read -p "Ingrese una URL de la cual descargar la imagen: " enlace_imagen
@@ -20,17 +15,8 @@ wget "$enlace_imagen" -qO "/app/archivos_generados/$nombre.jpg"
 wget "$enlace_texto" -qO "/app/archivos_generados/$nombre.txt"
 
 if [[ $(sha256sum "/app/archivos_generados/$nombre.jpg"|cut -d " " -f 1) == $(cat "/app/archivos_generados/$nombre.txt") ]]; then
-<<<<<<< HEAD
-        echo "Las sumas de verificación coinciden."
-=======
 	echo "Las sumas de verificación coinciden."
->>>>>>> rama_descargar
 else
         echo "Las sumas de verificación no coinciden."
         exit 1
 fi
-<<<<<<< HEAD
-
-
-=======
->>>>>>> rama_descargar
