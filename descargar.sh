20d331d6671f5952378d5ed7dea14d9c01279ef6
GusFontana404 <117466366+GusFontana404@users.noreply.github.com>
2023-07-28 23:10:54 -0300

    Cambio un tr por un sed

--- descargar.sh
+++ descargar.sh
@@ -3,7 +3,7 @@
 wget -qO "/app/archivos_generados/archivo.txt" https://raw.githubusercontent.com/adalessandro/EdP-2023-TP-Final/main/dict.csv
 generar_nombres(){
         archivo=$1
-        nombre=$(shuf -n 1 "$archivo"|cut -d "," -f 1|tr -s " "|tr " " "_"|tr "áéíóúÁÉÍÓÚ" "aeiouAEIOU")
+        nombre=$(shuf -n 1 "$archivo"|cut -d "," -f 1|tr -s " "|tr " " "_"|sed -i "s/[áéíóúÁÉÍÓÚ]/[aeiouAEIOU]/g")
 	echo "$nombre"
 }
 
