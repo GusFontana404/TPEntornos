d7aa53462f7e995ec7fc787f70e637f79e657d73
GusFontana404 <117466366+GusFontana404@users.noreply.github.com>
2023-07-28 21:42:01 -0300

    Arreglo error en la ruta

--- generar.sh
+++ generar.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-wget -qO "/app/datos_generados/archivo.txt"  https://raw.githubusercontent.com/adalessandro/EdP-2023-TP-Final/main/dict.csv
+wget -qO "/app/archivos_generados/archivo.txt"  https://raw.githubusercontent.com/adalessandro/EdP-2023-TP-Final/main/dict.csv
 
 generar_nombres(){
         archivo=$1
@@ -14,10 +14,10 @@ if cantidad == 0; then
 	exit 1
 else
 	for (( i=1; i<=cantidad; i++));do
-		nombre=$(generar_nombres "/app/datos_generados/archivo.txt")
-		wget -qO "/app/datos_generados/$nombre.jpg" https://thispersondoesnotexist.com/
-		gzip "/app/datos_generados/$nombre.jpg"
-		sha256sum "/app/datos_generados/$nombre.jpg.gz"| cut -d " " -f 1 > "/app/datos_generados/$nombre.txt"
+		nombre=$(generar_nombres "/app/archivos_generados/archivo.txt")
+		wget -qO "/app/archivos_generados/$nombre.jpg" https://thispersondoesnotexist.com/
+		gzip "/app/archivos_generados/$nombre.jpg"
+		sha256sum "/app/archivos_generados/$nombre.jpg.gz"| cut -d " " -f 1 > "/app/archivos_generados/$nombre.txt"
 		sleep 1
 	echo "Se generó correctamente la imagen"
 	done
